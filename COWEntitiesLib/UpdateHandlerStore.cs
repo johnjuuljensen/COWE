d5b0b72ebd9003696c589c0be05c450cb78fddd7
jjj <john.juul.jensen@hotmail.com>
2025-05-23 15:45:06 +0200

    Case insensitive handler store

--- COWEntitiesLib/UpdateHandlerStore.cs
+++ COWEntitiesLib/UpdateHandlerStore.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace COWEntities;
 
 public class HandlerStore<TExecutor> {
-    protected static readonly Dictionary<string, TExecutor> ms_handlers = new();
+    protected static readonly Dictionary<string, TExecutor> ms_handlers = new( StringComparer.InvariantCultureIgnoreCase );
 
     protected static bool RegisterHandler<T>( TExecutor executor, string? nameOverload = null ) where T : class {
-        return ms_handlers.TryAdd( nameOverload ?? typeof( T ).Name.ToUpperInvariant(), executor );
+        return ms_handlers.TryAdd( nameOverload ?? typeof( T ).Name, executor );
     }
 
     public delegate TResult DMapNoHandler<TResult>();
@@ -29,7 +30,7 @@ public class UpdateHandlerStore: HandlerStore<UpdateHandlerStore.IUpdateExecutor
         RegisterHandler<T>( new UpdateExecutor<T>(), nameOverload );
 
     public static Task<TResult> InvokeUpdateHandlerAsync<TResult>( string entityName, IUpdateContext<TResult> updateContext, DMapNoHandler<TResult> mapNoHandler ) =>
-        ms_handlers.TryGetValue( entityName.ToUpperInvariant(), out var handler )
+        ms_handlers.TryGetValue( entityName, out var handler )
         ? handler.RunAsync( updateContext )
         : Task.FromResult( mapNoHandler() );
 
